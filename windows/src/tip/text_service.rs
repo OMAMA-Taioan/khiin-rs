b2f41b24c234ba4d3d74ef4154f14e20602e6011
Aiong <aiongtaigi@gmail.com>
2023-03-28 20:34:10 +0800

    Use UnsafeCell for holding threadmgr

--- windows/src/tip/text_service.rs
+++ windows/src/tip/text_service.rs
@@ -1,7 +1,13 @@
 use core::option::Option;
 use std::cell::Cell;
+use std::cell::RefCell;
+use std::cell::UnsafeCell;
+use std::ffi::c_void;
+use std::os::windows::thread;
 use std::sync::atomic::AtomicUsize;
 use std::sync::Arc;
+
+use windows::core::Interface;
 use windows::core::implement;
 use windows::core::Result;
 use windows::Win32::UI::TextServices::ITfTextInputProcessor;
@@ -21,7 +27,18 @@ pub struct TextService {
     disp_attrs: DisplayAttributes,
     clientid: Cell<u32>,
     dwflags: Cell<u32>,
-    threadmgr: Cell<*const ITfThreadMgr>,
+
+    // The ITfThreadMgr comes from TSF in the Activate method
+    // We need this throughout the lifetime of the TextService,
+    // including in methods where we don't receive it again from Windows
+    // (e.g., we are expected to keep a handle to it for our own use)
+    // Windows guarantees to not delete it while we are using it
+    // However, we don't have a lifetime parameter to refer to,
+    // so there is no way to store the reference directly. Instead,
+    // we must use an UnsafeCell and the raw pointer. The pointer is
+    // set on activation and retrieved as a reference for use in
+    // all other functions.
+    threadmgr: UnsafeCell<*mut c_void>,
 }
 
 impl TextService {
@@ -31,7 +48,7 @@ impl TextService {
             disp_attrs: DisplayAttributes::new(),
             clientid: Cell::new(TF_CLIENTID_NULL),
             dwflags: Cell::new(0),
-            threadmgr: Cell::new(std::ptr::null()),
+            threadmgr: UnsafeCell::new(std::ptr::null_mut()),
         }
     }
 
@@ -44,17 +61,28 @@ impl TextService {
     }
 
     fn activate(&self, threadmgr: &ITfThreadMgr) -> Result<()> {
+        self.set_threadmgr(threadmgr);
         KeyEventSink::advise(self, threadmgr)?;
         Ok(())
     }
 
-    fn deactivate(&self, threadmgr: &ITfThreadMgr) -> Result<()> {
-        KeyEventSink::unadvise(self, threadmgr)?;
+    fn deactivate(&self) -> Result<()> {
+        if let Some(threadmgr) = self.threadmgr() {
+            KeyEventSink::unadvise(self, threadmgr)?;
+        }
+
         Ok(())
     }
 
-    fn threadmgr(&self) -> &ITfThreadMgr {
-        unsafe { &*self.threadmgr.get() }
+    fn threadmgr(&self) -> Option<&ITfThreadMgr> {
+        unsafe {
+            ITfThreadMgr::from_raw_borrowed(&*self.threadmgr.get())
+        }
+    }
+
+    fn set_threadmgr(&self, threadmgr: &ITfThreadMgr) {
+        let unsafe_cell = self.threadmgr.get();
+        unsafe { *unsafe_cell = threadmgr.clone().into_raw(); }
     }
 }
 
@@ -64,8 +92,7 @@ impl ITfTextInputProcessor_Impl for TextService {
     }
 
     fn Deactivate(&self) -> Result<()> {
-        self.deactivate(self.threadmgr())?;
-        self.threadmgr.set(std::ptr::null());
+        self.deactivate()?;
         Ok(())
     }
 }
@@ -81,10 +108,7 @@ impl ITfTextInputProcessorEx_Impl for TextService {
         self.dwflags.set(dwflags);
 
         match ptim {
-            Some(threadmgr) => {
-                self.threadmgr.set(&*threadmgr);
-                self.activate(threadmgr)
-            },
+            Some(threadmgr) => self.activate(threadmgr),
             None => Ok(()),
         }
     }
