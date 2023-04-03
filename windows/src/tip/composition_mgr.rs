6e54b1b53a12b434983235ba8fa8e0e036be0bd2
Aiong <aiongtaigi@gmail.com>
2023-04-03 15:33:34 +0800

    wip composition

--- windows/src/tip/composition_mgr.rs
+++ windows/src/tip/composition_mgr.rs
@@ -1,10 +1,8 @@
 use std::mem::ManuallyDrop;
 use std::sync::Arc;
 
+use khiin_protos::command::CommandType;
 use khiin_protos::command::Preedit;
-use windows::Win32::UI::TextServices::TF_AE_END;
-use windows::Win32::UI::TextServices::TF_ANCHOR_START;
-use windows::Win32::UI::TextServices::TF_ST_CORRECTION;
 use windows::core::ComInterface;
 use windows::core::Result;
 use windows::Win32::Foundation::FALSE;
@@ -15,10 +13,13 @@ use windows::Win32::UI::TextServices::ITfContextComposition;
 use windows::Win32::UI::TextServices::ITfInsertAtSelection;
 use windows::Win32::UI::TextServices::ITfRange;
 use windows::Win32::UI::TextServices::TfActiveSelEnd;
+use windows::Win32::UI::TextServices::TF_AE_END;
 use windows::Win32::UI::TextServices::TF_AE_NONE;
+use windows::Win32::UI::TextServices::TF_ANCHOR_START;
 use windows::Win32::UI::TextServices::TF_IAS_QUERYONLY;
 use windows::Win32::UI::TextServices::TF_SELECTION;
 use windows::Win32::UI::TextServices::TF_SELECTIONSTYLE;
+use windows::Win32::UI::TextServices::TF_ST_CORRECTION;
 
 use khiin_protos::command::Command;
 
@@ -45,7 +46,24 @@ impl CompositionMgr {
         }
 
         if let Some(comp) = self.composition.clone() {
-            self.do_composition(ec, comp, context, &command.response.preedit)?;
+            if command.response.committed
+                || command.request.type_.enum_value_or_default()
+                    == CommandType::CMD_COMMIT
+            {
+                self.commit_composition(
+                    ec,
+                    comp,
+                    context,
+                    &command.response.preedit,
+                )?;
+            } else {
+                self.do_composition(
+                    ec,
+                    comp,
+                    context,
+                    &command.response.preedit,
+                )?;
+            }
         }
 
         Ok(())
@@ -70,7 +88,7 @@ impl CompositionMgr {
     }
 
     fn do_composition(
-        &self,
+        &mut self,
         ec: u32,
         composition: ITfComposition,
         context: ITfContext,
@@ -85,23 +103,75 @@ impl CompositionMgr {
             }
 
             let range = composition.GetRange()?;
-            range.SetText(ec, TF_ST_CORRECTION, &preedit.display);
+            let display = preedit.display.clone();
+            range.SetText(ec, TF_ST_CORRECTION, &display);
 
             // TODO segment attrs
 
             let curs_range = range.Clone()?;
             curs_range.Collapse(ec, TF_ANCHOR_START);
             let mut shifted: i32 = 0;
-            curs_range.ShiftEnd(ec, preedit.caret, &mut shifted, std::ptr::null())?;
-            curs_range.ShiftStart(ec, preedit.caret, &mut shifted, std::ptr::null())?;
+            curs_range.ShiftEnd(
+                ec,
+                preedit.caret,
+                &mut shifted,
+                std::ptr::null(),
+            )?;
+            curs_range.ShiftStart(
+                ec,
+                preedit.caret,
+                &mut shifted,
+                std::ptr::null(),
+            )?;
             self.set_selection(ec, context, curs_range, TF_AE_END)?;
 
             Ok(())
         }
     }
 
+    fn commit_composition(
+        &mut self,
+        ec: u32,
+        composition: ITfComposition,
+        context: ITfContext,
+        preedit: &Preedit,
+    ) -> Result<()> {
+        unsafe {
+            let preedit = preedit.widen();
+            let range = composition.GetRange()?;
+            let end_range = range.Clone()?;
+            let mut shifted: i32 = 0;
+            end_range.ShiftStart(
+                ec,
+                preedit.display.len() as i32,
+                &mut shifted,
+                std::ptr::null(),
+            )?;
+            end_range.Collapse(ec, TF_ANCHOR_START);
+            composition.ShiftStart(ec, &end_range);
+            self.set_selection(ec, context, end_range, TF_AE_END)?;
+            self.cleanup(ec, composition)?;
+        }
+        Ok(())
+    }
+
+    fn cancel_composition(&mut self, ec: u32) -> Result<()> {
+        match self.composition.clone() {
+            Some(comp) => self.cleanup(ec, comp),
+            _ => Ok(())
+        }
+    }
+
+    fn cleanup(&mut self, ec: u32, composition: ITfComposition) -> Result<()> {
+        unsafe {
+            composition.EndComposition(ec)?;
+        }
+        self.composition = None;
+        Ok(())
+    }
+
     fn set_selection(
-        &self,
+        &mut self,
         ec: u32,
         context: ITfContext,
         range: ITfRange,
