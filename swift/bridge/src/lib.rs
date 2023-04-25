97438c6ebe9c06a1a51a5fdd4dddf7c39af7490c
aiongg <aiongtaigi@gmail.com>
2023-04-25 12:29:07 +0800

    Use fork of swift-bridge with vec as_ptr

--- swift/bridge/src/lib.rs
+++ swift/bridge/src/lib.rs
@@ -1,7 +1,4 @@
-use std::ffi::c_char;
 use std::ffi::c_void;
-use std::ffi::CStr;
-use std::slice;
 
 use khiin::Engine;
 
@@ -48,27 +45,3 @@ impl EngineController {
         engine.send_command_bytes(cmd_input).ok()
     }
 }
-
-// #[no_mangle]
-// pub extern "C" fn Rust_khiin_engine_send_command(
-//     engine_ptr: *mut c_void,
-// cmd_input: *const u8,
-// len_input: usize,
-// cmd_output: *mut *mut u8,
-// len_output: *mut usize,
-// ) -> i32 {
-//     let engine: &mut Engine = unsafe { &mut *(engine_ptr as *mut Engine) };
-//     let bytes = unsafe { slice::from_raw_parts(cmd_input, len_input) };
-//     let mut res_bytes = match engine.send_command_bytes(bytes) {
-//         Ok(bytes) => bytes,
-//         Err(_) => return 1,
-//     };
-
-//     unsafe {
-//         *len_output = res_bytes.len();
-//         *cmd_output = res_bytes.as_mut_ptr();
-//     }
-
-//     std::mem::forget(res_bytes);
-//     0
-// }
