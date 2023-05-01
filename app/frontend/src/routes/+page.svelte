1a8d976ad5f0ae29bb1737529df42d6d806a9850
Aiong <aiongtaigi@gmail.com>
2023-05-01 14:26:04 +0800

    settings app wip

--- app/frontend/src/routes/+page.svelte
+++ app/frontend/src/routes/+page.svelte
@@ -2,8 +2,16 @@
     import Settings from "../lib/Settings.svelte";
 </script>
 
-<h1>Welcome to ŏ͘ ba̍h</h1>
+<h1>Khíín 台語打字法</h1>
+<img src="/app-icon.png" />
 <p>
     Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
 <Settings />
+
+<style>
+    img {
+        width: 6rem;
+        height: 6rem;
+    }
+</style>
