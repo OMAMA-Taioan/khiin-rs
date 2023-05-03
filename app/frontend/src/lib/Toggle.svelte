13d40a76d3b4e6b9518c52dde4afca2922bd7b7a
aresastro <dean@bitping.com>
2023-05-03 11:49:40 +0800

    Added input settings page

---
+++ app/frontend/src/lib/Toggle.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    export let checked: boolean = false;
+    function toggle() {
+        checked = !checked;
+    }
+</script>
+<div class="w-10 h-6 flex items-center {checked ? 'bg-slate-500' : 'bg-gray-300'} rounded-full px-1 duration-200" on:click="{toggle}">
+    <div class="bg-white w-4 h-4 rounded-full shadow-md transform {checked && 'translate-x-4'} duration-200"></div>
+</div>
--- app/frontend/src/routes/appearance/+page.svelte
+++ app/frontend/src/routes/appearance/+page.svelte
@@ -19,32 +19,32 @@
               <option>{$_('page.appearance.dark')}</option>
             </select>
           </label>
-    <label class="block">
-        <span class="text-gray-700">{$_('page.appearance.language')}</span>
-        <select class="block w-full mt-1 rounded-md border-slate-300 shadow-sm focus:border-slate-300 focus:ring focus:ring-slate-200 focus:ring-opacity-50" on:change={updateLanguage}>
-          <option value="en">English</option>
-          <option value="zh-TW">漢羅</option>
-          <option value="oan-TW">Lô-má-jī</option>
-        </select>
-      </label>
-      <label class="block">
-        <span class="text-gray-700">{$_('page.appearance.font-size')}</span>
-        <select class="block w-full mt-1 rounded-md border-slate-300 shadow-sm focus:border-slate-300 focus:ring focus:ring-slate-200 focus:ring-opacity-50">
-            <option>{$_('page.appearance.xs')}</option>
-            <option>{$_('page.appearance.sm')}</option>
-            <option>{$_('page.appearance.md')}</option>
-            <option>{$_('page.appearance.lg')}</option>
-            <option>{$_('page.appearance.xl')}</option>
-          </select>
-      </label>
-      <label class="block">
-            <span class="text-gray-700">{$_('page.appearance.font')}</span>
-            <select class="block w-full mt-1 rounded-md border-slate-300 shadow-sm focus:border-slate-300 focus:ring focus:ring-slate-200 focus:ring-opacity-50">
-                <!-- TODO: Local these fonts -->
-                <option>Arial</option>
-                <option>Helvetica</option>
-                <option>黑體</option>
-            </select>  
-      </label>
+          <label class="block">
+              <span class="text-gray-700">{$_('page.appearance.language')}</span>
+              <select class="block w-full mt-1 rounded-md border-slate-300 shadow-sm focus:border-slate-300 focus:ring focus:ring-slate-200 focus:ring-opacity-50" on:change={updateLanguage}>
+                <option value="en">English</option>
+                <option value="zh-TW">漢羅</option>
+                <option value="oan-TW">Lô-má-jī</option>
+              </select>
+            </label>
+            <label class="block">
+              <span class="text-gray-700">{$_('page.appearance.font-size')}</span>
+              <select class="block w-full mt-1 rounded-md border-slate-300 shadow-sm focus:border-slate-300 focus:ring focus:ring-slate-200 focus:ring-opacity-50">
+                  <option>{$_('page.appearance.xs')}</option>
+                  <option>{$_('page.appearance.sm')}</option>
+                  <option>{$_('page.appearance.md')}</option>
+                  <option>{$_('page.appearance.lg')}</option>
+                  <option>{$_('page.appearance.xl')}</option>
+                </select>
+            </label>
+            <label class="block">
+                  <span class="text-gray-700">{$_('page.appearance.font')}</span>
+                  <select class="block w-full mt-1 rounded-md border-slate-300 shadow-sm focus:border-slate-300 focus:ring focus:ring-slate-200 focus:ring-opacity-50">
+                      <!-- TODO: Local these fonts -->
+                      <option>Arial</option>
+                      <option>Helvetica</option>
+                      <option>黑體</option>
+                  </select>  
+            </label>
     </div>
 </div>
