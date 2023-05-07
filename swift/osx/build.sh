28075867c379b0dea8e4c0a267b40025cd8e3fb2
aiongg <aiongtaigi@gmail.com>
2023-05-07 20:59:06 +0800

    add osx package installer

--- swift/osx/build.sh
+++ swift/osx/build.sh
@@ -13,35 +13,42 @@ else
 fi
 
 app_name=KhiinPJH
-triple=arm64-apple-macosx
-build_dir=.build/$triple/$BUILD_DIR
+build_dir=.build/$BUILD_DIR
+universal_dir=.build/universal-macosx/$BUILD_DIR
 assets_dir=assets
 bundle_dir=$build_dir/$app_name.app
 contents_dir=$bundle_dir/Contents
 bin_dir=$contents_dir/MacOS
 res_dir=$contents_dir/Resources
-icon_dir=$build_dir/AppIcon.iconset
+icon_dir=.build/icons
+iconset_dir=$icon_dir/AppIcon.iconset
 im_dir="/Users/$(id -un)/Library/Input Methods/"
 db_file=../../resources/khiin.db
 
 # Prepare icon assets
-rm -rf $icon_dir
-mkdir -p $icon_dir
+rm -rf $iconset_dir
+mkdir -p $iconset_dir
 icon_src=$assets_dir/main_icon_512.png
-sips -z 16 16     $icon_src --out $icon_dir/icon_16x16.png
-sips -z 32 32     $icon_src --out $icon_dir/icon_16x16@2x.png
-sips -z 32 32     $icon_src --out $icon_dir/icon_32x32.png
-sips -z 64 64     $icon_src --out $icon_dir/icon_32x32@2x.png
-sips -z 128 128   $icon_src --out $icon_dir/icon_128x128.png
-sips -z 256 256   $icon_src --out $icon_dir/icon_128x128@2x.png
-sips -z 256 256   $icon_src --out $icon_dir/icon_256x256.png
-sips -z 512 512   $icon_src --out $icon_dir/icon_256x256@2x.png
-cp                $icon_src       $icon_dir/icon_512x512.png
-iconutil -c icns $icon_dir
-icns_file=$build_dir/AppIcon.icns
+sips -z 16 16     $icon_src --out $iconset_dir/icon_16x16.png
+sips -z 32 32     $icon_src --out $iconset_dir/icon_16x16@2x.png
+sips -z 32 32     $icon_src --out $iconset_dir/icon_32x32.png
+sips -z 64 64     $icon_src --out $iconset_dir/icon_32x32@2x.png
+sips -z 128 128   $icon_src --out $iconset_dir/icon_128x128.png
+sips -z 256 256   $icon_src --out $iconset_dir/icon_128x128@2x.png
+sips -z 256 256   $icon_src --out $iconset_dir/icon_256x256.png
+sips -z 512 512   $icon_src --out $iconset_dir/icon_256x256@2x.png
+cp                $icon_src       $iconset_dir/icon_512x512.png
+iconutil -c icns $iconset_dir
+icns_file=$icon_dir/AppIcon.icns
 
 # Build the application
-swift build --configuration $BUILD_FLAG --triple $triple
+swift build --configuration $BUILD_FLAG --triple arm64-apple-macosx
+swift build --configuration $BUILD_FLAG --triple x86_64-apple-macosx
+mkdir -p $universal_dir
+lipo \
+    .build/arm64-apple-macosx/$BUILD_DIR/$app_name \
+    .build/x86_64-apple-macosx/$BUILD_DIR/$app_name -create -output \
+    $universal_dir/$app_name
 
 # Bundle it into .app
 rm -rf $bundle_dir
@@ -49,15 +56,15 @@ mkdir -p $bundle_dir
 mkdir -p $contents_dir
 mkdir -p $bin_dir
 mkdir -p $res_dir
-cp $build_dir/$app_name         $bin_dir
-cp $assets_dir/Info.plist       $contents_dir
-cp $assets_dir/PkgInfo          $contents_dir
-cp $icns_file                   $res_dir
-cp $assets_dir/main.svg         $res_dir
-cp $assets_dir/menuicon.svg     $res_dir
-cp -r $assets_dir/en.lproj      $res_dir
-cp -r $assets_dir/zh-Hant.lproj $res_dir
-cp $db_file                     $res_dir
+cp $universal_dir/$app_name         $bin_dir
+cp $assets_dir/Info.plist           $contents_dir
+cp $assets_dir/PkgInfo              $contents_dir
+cp $icns_file                       $res_dir
+cp $assets_dir/main.svg             $res_dir
+cp $assets_dir/menuicon.svg         $res_dir
+cp -r $assets_dir/en.lproj          $res_dir
+cp -r $assets_dir/zh-Hant.lproj     $res_dir
+cp $db_file                         $res_dir
 
 # Move it to the user's input method folder
 killall -9 $app_name || true
@@ -65,3 +72,12 @@ cp -r $bundle_dir "$im_dir"
 ls -la "$im_dir"
 echo "$app_name.app successfully installed to ~/Library/Input Methods"
 echo "You may need to log out and in to see it in the System Settings."
+
+pkgbuild \
+    --info assets/PackageInfo \
+    --root $build_dir/KhiinPJH.app \
+    --identifier app.khiin.inputmethod.khiin \
+    --version "0.1.0" \
+    --install-location "/tmp/KhiinPJH.app" \
+    --scripts assets/scripts \
+    "$build_dir/KhiinPJH.pkg"
