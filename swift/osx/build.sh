9b82d39cf64ec798b4e13f7d617735d5984aecd6
aiongg <aiongtaigi@gmail.com>
2023-05-07 21:35:53 +0800

    upload artifact debugging

--- swift/osx/build.sh
+++ swift/osx/build.sh
@@ -42,13 +42,13 @@ iconutil -c icns $iconset_dir
 icns_file=$icon_dir/AppIcon.icns
 
 # Build the application
-swift build --configuration $BUILD_FLAG --triple arm64-apple-macosx
+# swift build --configuration $BUILD_FLAG --triple arm64-apple-macosx
 swift build --configuration $BUILD_FLAG --triple x86_64-apple-macosx
 mkdir -p $universal_dir
 lipo \
-    .build/arm64-apple-macosx/$BUILD_DIR/$app_name \
     .build/x86_64-apple-macosx/$BUILD_DIR/$app_name -create -output \
     $universal_dir/$app_name
+    # .build/arm64-apple-macosx/$BUILD_DIR/$app_name \
 
 # Bundle it into .app
 rm -rf $bundle_dir
@@ -81,3 +81,5 @@ pkgbuild \
     --install-location "/tmp/KhiinPJH.app" \
     --scripts assets/scripts \
     "$build_dir/KhiinPJH.pkg"
+
+pwd
